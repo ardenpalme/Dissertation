4b8f2e5063d89775810b82e3901ad23002acd3e2
Ubuntu <ubuntu@ip-172-31-46-77.eu-west-1.compute.internal>
2026-08-13 23:23:57 +0000

    ready for next sim

--- lib/simulation.py
+++ lib/simulation.py
@@ -31,7 +31,8 @@ def clf_stage(g, config_g, run_dir, b):
     gf = GraphFactory(config_g['train']['num_nodes'], b)
     clf = ByzClassifier(config_g, gd, gf)
 
-    pkl_file = os.path.join(run_dir, f"clf_sim_data_{g}_{b}.pkl.gz")
+    os.makedirs(os.path.join(run_dir, 'clf_sim_data'), exist_ok=True)
+    pkl_file = os.path.join(run_dir, 'clf_sim_data', f"clf_sim_data_{g}_{b}.pkl.gz")
     if os.path.exists(pkl_file):
         proj_const = load_run(clf, pkl_file)['proj_const']
     else:
@@ -60,8 +61,7 @@ def sim_task(g, config_g, atk, s, proj_const, preproc, est, gamma_sys, rdsgd_par
 
     stationary_dist = (1 - gamma_sys) * config_g['pi']
     config_g['graph_args']['MH_target_pi'] = stationary_dist
-    #config_g['oracle_params'].append({'C_fpr':rdsgd_params['C_fpr'], 'C_fnr':rdsgd_params['C_fnr']})
-    #algs.append('ORACLE_2')
+
     ss.init_simulation(config_g, proj_const, preproc, est, config_g['oracle_params'], rdsgd_params, seed('sys', s), is_printing_logs=False)
     df = ss.simulate(dict(algorithms=algs, atk_type=atk, threat_model='T3', seed=s))
     
